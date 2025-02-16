b05d564b7fdebec7805a4b990345ed306f83f401
a.pashkevich <pashkevich.anton.v@gmail.com>
2025-02-16 21:26:58 +0300

    еще изсенения

--- sitewife/test_app1/static/js/animations.js
+++ sitewife/test_app1/static/js/animations.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function () {
     const aboutSections = document.querySelectorAll(".about-me");
 
@@ -6,20 +5,40 @@ document.addEventListener("DOMContentLoaded", function () {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("show");
-                entry.target.classList.add("visible"); // Фикс для верхнего блока
             } else {
-                // Убираем только если пользователь **прокрутил далеко вниз**
-                setTimeout(() => {
-                    if (!entry.isIntersecting && entry.target.classList.contains("visible")) {
-                        entry.target.classList.remove("show");
-                    }
-                }, 500); // Задержка перед скрытием (можно увеличить)
+                entry.target.classList.remove("show"); // Убираем класс, если блок уходит из зоны видимости
             }
         });
-    }, { threshold: 0.3 }); // Изменено на 0.6, чтобы не срабатывало слишком часто
+    }, { threshold: 0.2 }); // Можно настроить порог видимости
 
     aboutSections.forEach(section => {
         observer.observe(section);
     });
 });
 
+
+
+//document.addEventListener("DOMContentLoaded", function () {
+//    const aboutSections = document.querySelectorAll(".about-me");
+//
+//    const observer = new IntersectionObserver((entries) => {
+//        entries.forEach(entry => {
+//            if (entry.isIntersecting) {
+//                entry.target.classList.add("show");
+//                entry.target.classList.add("visible"); // Фикс для верхнего блока
+//            } else {
+//                // Убираем только если пользователь **прокрутил далеко вниз**
+//                setTimeout(() => {
+//                    if (!entry.isIntersecting && entry.target.classList.contains("visible")) {
+//                        entry.target.classList.remove("show");
+//                    }
+//                }, 900); // Задержка перед скрытием (можно увеличить)
+//            }
+//        });
+//    }, { threshold: 0.3 }); // Изменено на 0.6, чтобы не срабатывало слишком часто
+//
+//    aboutSections.forEach(section => {
+//        observer.observe(section);
+//    });
+//});
+//
