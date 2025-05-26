6878817eaa310971a0d34f4cc7c2b1fdbf29dd28
a.pashkevich <pashkevich.anton.v@gmail.com>
2025-05-26 15:36:30 +0300

    Последняя версия актуальная от 26.05.2025

--- sitewife/test_app1/static/js/animations.js
+++ sitewife/test_app1/static/js/animations.js
@@ -1,17 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const aboutSections = document.querySelectorAll(".about-me");
+    // 1. Плавное появление about-me и about-me-combined
+    const aboutSections = document.querySelectorAll(".about-me, .about-me-combined, .work-section");
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add("show");
             } else {
-                entry.target.classList.remove("show"); // Убираем класс, если блок уходит из зоны видимости
+                entry.target.classList.remove("show");
             }
         });
-    }, { threshold: 0.2 }); // Можно настроить порог видимости
+    }, { threshold: 0.2 });
 
     aboutSections.forEach(section => {
         observer.observe(section);
     });
+
+    // 2. Анимация выезда только для .mainup
+    const mainupSection = document.querySelector(".mainup");
+
+    if (mainupSection) {
+        const mainupObserver = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.add("show");
+                } else {
+                    entry.target.classList.remove("show");
+                }
+            });
+        }, { threshold: 0.3 });
+
+        mainupObserver.observe(mainupSection);
+    }
 });
--- sitewife/test_app1/static/js/invisible.js
+++ sitewife/test_app1/static/js/invisible.js
@@ -3,11 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const rightsContent = document.querySelector('.rights-content');
 
     toggleButton.addEventListener('click', function () {
-        if (rightsContent.classList.contains('show')) {
-            rightsContent.style.maxHeight = null; // Закрываем блок
-        } else {
-            rightsContent.style.maxHeight = rightsContent.scrollHeight + "px"; // Устанавливаем высоту контента
-        }
         rightsContent.classList.toggle('show');
     });
 });
+
