f4a69d9c0d4479ae95ddd827d6e5b83123b773de
a.pashkevich <pashkevich.anton.v@gmail.com>
2025-06-03 16:16:08 +0300

    последние правки в оформление для мобильных устройств. Сделал перенос контактов.

--- sitewife/sitewife/settings.py
+++ sitewife/sitewife/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hdq&uo2(99f9-u61sy&3j2u)wz+7@s%v%a#)4sjy&!e=tp(kf)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
